798270c6e4b41f0dec8be01cbf508f0d027a6c2d
Andrew Reid East <andrew@andreweast.net>
2016-11-06 12:43:15 +0000

    Make navbar dynamically generated

---
+++ client/navbar.js
@@ -0,0 +1,9 @@
+Template.navbar.helpers({
+  navMenu: [
+    {display: 'Home', link: 'index'},
+    {display: 'Button', link: 'button'},
+    {display: 'About', link: 'about'},
+    {display: 'Find Us', link: 'findUs'},
+    {display: 'Contact', link: 'contact'}
+  ],
+});
